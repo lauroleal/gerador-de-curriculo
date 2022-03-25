cda74da910a61d72847ab2cc287c9201b7b1ff53
Lauro <lauroleal2011@gmail.com>
2022-03-25 03:40:37 -0300

    subindo a versão  preliminar

--- action.js
+++ action.js
@@ -1,37 +1,44 @@
-const form = document.querySelector("form");
+// const form = document.querySelector("form");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+// form.addEventListener("submit")
+//     //event.preventDefault();
 
-    const titulo = document.getElementById("campo__titulo");
-    const titulo__val = document.createTextNode(titulo.value);
+// const nome = document.getElementById("name").value;
+// console.log(nome.toUpperCase());
 
-    const descricao = document.getElementById("campo__descricao");
-    const descricao__val = document.createTextNode(descricao.value);
+// //let nome = document.getElementById('#name').value;
+// document.getElementById("#texto__nome").innerHTML += nome;
 
-    const img = document.getElementById("campo__imagem");
-    const img__val = document.createTextNode(img.value);
+//     const nome = document.getElementById("name").value;
+//     console.log(nome.toUpperCase());
+//     // const titulo__val = document.createTextNode(titulo.value);
+
+//     const campo__nome = document.getElementById("texto__nome");
+//     campo__nome.innerHTML = nome.value;
+// const descricao__val = document.createTextNode(descricao.value);
+
+// const img = document.getElementById("campo__imagem");
+// const img__val = document.createTextNode(img.value);
 
 
-    const post = document.getElementById("postagem");
-    const section = document.createElement("section");
-    section.classList.add("box__externo__post");
+// const post = document.getElementById("postagem");
+// const section = document.createElement("section");
+// section.classList.add("box__externo__post");
 
-    const campo__imagem = document.createElement("img");
-    campo__imagem.classList.add("img__post");
-    campo__imagem.setAttribute('src', img.value);
-    campo__imagem.appendChild(img__val);
+// const campo__imagem = document.createElement("img");
+// campo__imagem.classList.add("img__post");
+// campo__imagem.setAttribute('src', img.value);
+// campo__imagem.appendChild(img__val);
 
-    const campo__titulo = document.createElement("p");
-    campo__titulo.classList.add("p__titulo");
-    campo__titulo.appendChild(titulo__val);
+// const campo__titulo = document.createElement("p");
+// campo__titulo.classList.add("p__titulo");
+// campo__titulo.appendChild(titulo__val);
 
-    const campo__descricao = document.createElement("p");
-    campo__descricao.classList.add("p__texto");
-    campo__descricao.appendChild(descricao__val);
+// const campo__descricao = document.createElement("p");
+// campo__descricao.classList.add("p__texto");
+// campo__descricao.appendChild(descricao__val);
 
-    section.appendChild(campo__imagem);
-    section.appendChild(campo__titulo);
-    section.appendChild(campo__descricao);
-    post.appendChild(section);
-});
+// section.appendChild(campo__imagem);
+// section.appendChild(campo__titulo);
+// section.appendChild(campo__descricao);
+// post.appendChild(section);
