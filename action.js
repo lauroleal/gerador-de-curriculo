30cd1f38f2199fa8f3500ce9d540310ca5890ea1
Lauro <lauroleal2011@gmail.com>
2022-03-25 17:24:44 -0300

    Uu ta faltando configurar o arquivo de saida apenas!

--- action.js
+++ action.js
@@ -1,44 +1,279 @@
-// const form = document.querySelector("form");
-
-// form.addEventListener("submit")
-//     //event.preventDefault();
-
-// const nome = document.getElementById("name").value;
-// console.log(nome.toUpperCase());
-
-// //let nome = document.getElementById('#name').value;
-// document.getElementById("#texto__nome").innerHTML += nome;
-
-//     const nome = document.getElementById("name").value;
-//     console.log(nome.toUpperCase());
-//     // const titulo__val = document.createTextNode(titulo.value);
-
-//     const campo__nome = document.getElementById("texto__nome");
-//     campo__nome.innerHTML = nome.value;
-// const descricao__val = document.createTextNode(descricao.value);
-
-// const img = document.getElementById("campo__imagem");
-// const img__val = document.createTextNode(img.value);
+let formulario = new URLSearchParams(location.search);
 
 
-// const post = document.getElementById("postagem");
-// const section = document.createElement("section");
-// section.classList.add("box__externo__post");
+formulario.forEach(function(valor, key) {
+    console.log(valor, key);
+    if (typeof valor !== 'undefined') {
+        if (key == 'name') {
+            document.getElementById('texto__nome').innerHTML = valor.toUpperCase();
+        }
+        if (key == 'nacionalidade') {
+            document.getElementById('linha__perfil').innerHTML = valor;
 
-// const campo__imagem = document.createElement("img");
-// campo__imagem.classList.add("img__post");
-// campo__imagem.setAttribute('src', img.value);
-// campo__imagem.appendChild(img__val);
+        }
+        if (key == 'estado__civil') {
+            let campo = document.getElementById('linha__perfil').innerText;
+            let valor__campo = `${campo}, ${valor}`;
+            document.getElementById('linha__perfil').innerHTML = valor__campo;
+        }
+        if (key == 'data') {
+            let campo = document.getElementById('linha__perfil').innerText;
+            // pegando a data atual
+            var dataAtual = new Date();
+            var dia = dataAtual.getDate();
+            var mes = (dataAtual.getMonth() + 1);
+            var ano = dataAtual.getFullYear();
+            // trabalhando com a data fornecida pelo usuario
+            let data = valor.split('-');
+            let dia__usuario = data[2];
+            let mes__usuario = data[1];
+            let ano__usuario = data[0];
 
-// const campo__titulo = document.createElement("p");
-// campo__titulo.classList.add("p__titulo");
-// campo__titulo.appendChild(titulo__val);
+            if (mes > mes__usuario || mes == mes__usuario && dia > dia__usuario) {
+                let idade = ano - ano__usuario;
+                let valor__campo = `${campo}, ${idade} anos`;
+                document.getElementById('linha__perfil').innerHTML = valor__campo;
 
-// const campo__descricao = document.createElement("p");
-// campo__descricao.classList.add("p__texto");
-// campo__descricao.appendChild(descricao__val);
+            } else {
+                let idade = (ano - ano__usuario) - 1;
+                let valor__campo = `${campo}, ${idade} anos`;
+                document.getElementById('linha__perfil').innerHTML = valor__campo;
+            }
+        }
+        if (key == 'cidade') {
+            document.getElementById('linha__endereco').innerHTML = valor;
+            console.log(valor);
+        }
+        if (key == 'bairro') {
+            let campo = document.getElementById('linha__endereco').innerText;
+            let valor__campo = `${valor} – ${campo}`;
+            document.getElementById('linha__endereco').innerHTML = valor__campo;
+        }
+        if (key == 'uf') {
+            let campo = document.getElementById('linha__endereco').innerText;
+            let valor__campo = `${campo} - ${valor}`;
+            document.getElementById('linha__endereco').innerHTML = valor__campo;
+        }
+        if (key == 'telefone') {
+            let tel = valor.split('');
+            document.getElementById('linha__telefone').innerHTML = `Cel. (${tel[0]}${tel[1]}) ${tel[2]} ${tel[3]}${tel[4]}${tel[5]}${tel[6]} - ${tel[7]}${tel[8]}${tel[9]}${tel[10]}`;
+        }
+        if (key == 'email') {
+            document.getElementById('linha__email').innerHTML += `E-mail: ${valor}`;
+        }
+        if (key == 'resumo') {
+            document.getElementById('linha__resumo').innerHTML = valor;
+        }
 
-// section.appendChild(campo__imagem);
-// section.appendChild(campo__titulo);
-// section.appendChild(campo__descricao);
-// post.appendChild(section);
+
+        if (key == 'escola__nome') {
+            document.getElementById('linha__forma').innerHTML = valor;
+        }
+        if (key == 'nome__curso') {
+            let form = document.getElementById('linha__forma').innerHTML;
+            let valor__campo = `<span class="bold">${valor}</span>, ${form}`;
+            document.getElementById('linha__forma').innerHTML = valor__campo;
+        }
+        if (key == 'situação__formacao__1') {
+            let form = document.getElementById('linha__forma').innerHTML;
+            let valor__campo = `${form} — ${valor}`;
+            document.getElementById('linha__forma').innerHTML = valor__campo;
+        }
+
+
+        if (key == 'escola__nome__2') {
+            document.getElementById('linha__forma2').innerHTML = valor;
+        }
+        if (key == 'nome__curso__2') {
+            let form = document.getElementById('linha__forma2').innerHTML;
+            let valor__campo = `<span class="bold">${valor}</span>, ${form}`;
+            document.getElementById('linha__forma2').innerHTML = valor__campo;
+        }
+        if (key == 'situação__formacao__2') {
+            let form = document.getElementById('linha__forma2').innerHTML;
+            let valor__campo = `${form} — ${valor}`;
+            document.getElementById('linha__forma2').innerHTML = valor__campo;
+        }
+        // uu pra evitar formação vazio
+        if (key == 'nome__curso__2') {
+            if (valor == '') {
+                document.getElementById('linha__forma2').innerHTML = '';
+            }
+        }
+
+
+        // curso 1
+        if (key == 'local__curso1') {
+            document.getElementById('linha__curso').innerHTML = valor;
+        }
+        if (key == 'nome__curso1') {
+            let form = document.getElementById('linha__curso').innerHTML;
+            let valor__campo = `<span class="bold">${valor}</span>, ${form}`;
+            document.getElementById('linha__curso').innerHTML = valor__campo;
+        }
+        if (key == 'situação__curso__1') {
+            let form = document.getElementById('linha__curso').innerHTML;
+            let valor__campo = `${form} — ${valor}`;
+            document.getElementById('linha__curso').innerHTML = valor__campo;
+        }
+
+        // uu pra evitar curso 1 vazio
+        if (key == 'nome__curso1') {
+            if (valor == '') {
+                document.getElementById('linha__curso').innerHTML = '';
+            }
+        }
+        // curso 2
+        if (key == 'local__curso2') {
+            document.getElementById('linha__curso2').innerHTML = valor;
+        }
+        if (key == 'nome__curso2') {
+            let form = document.getElementById('linha__curso2').innerHTML;
+            let valor__campo = `<span class="bold">${valor}</span>, ${form}`;
+            document.getElementById('linha__curso2').innerHTML = valor__campo;
+        }
+        if (key == 'situação__curso__2') {
+            let form = document.getElementById('linha__curso2').innerHTML;
+            let valor__campo = `${form} — ${valor}`;
+            document.getElementById('linha__curso2').innerHTML = valor__campo;
+        }
+        // uu pra evitar curso 1 vazio
+        if (key == 'nome__curso2') {
+            if (valor == '') {
+                document.getElementById('linha__curso2').innerHTML = '';
+            }
+        }
+
+        // curso 3
+        if (key == 'local__curso3') {
+            document.getElementById('linha__curso3').innerHTML = valor;
+        }
+        if (key == 'nome__curso3') {
+            let form = document.getElementById('linha__curso3').innerHTML;
+            let valor__campo = `<span class="bold">${valor}</span>, ${form}`;
+            document.getElementById('linha__curso3').innerHTML = valor__campo;
+        }
+        if (key == 'situação__curso__3') {
+            let form = document.getElementById('linha__curso3').innerHTML;
+            let valor__campo = `${form} — ${valor}`;
+            document.getElementById('linha__curso3').innerHTML = valor__campo;
+        }
+        // uu pra evitar curso 1 vazio
+        if (key == 'nome__curso3') {
+            if (valor == '') {
+                document.getElementById('linha__curso3').innerHTML = '';
+            }
+        }
+        if (key == 'objetivo') {
+            document.getElementById('linha__objetivo').innerHTML = valor;
+        }
+
+        // empresa 1
+        if (key == 'name_empresa__1') {
+            document.getElementById('linha__historico').innerHTML = valor;
+        }
+        if (key == 'cargo_empresa__1') {
+            let form = document.getElementById('linha__historico').innerHTML;
+            let valor__campo = `<span class="bold">● ${form}</span><span class="italico"> — ${valor}</span>`;
+            document.getElementById('linha__historico').innerHTML = valor__campo;
+        }
+        if (key == 'inicio_empresa__1') {
+            let form = document.getElementById('linha__historico').innerHTML;
+            let valor__campo = `${form} de ${valor}`;
+            document.getElementById('linha__historico').innerHTML = valor__campo;
+        }
+        if (key == 'saida_empresa__1') {
+            let form = document.getElementById('linha__historico').innerHTML;
+            let valor__campo = `${form} a ${valor}`;
+            document.getElementById('linha__historico').innerHTML = valor__campo;
+        }
+        if (key == 'resumo__empresa1') {
+            document.getElementById('linha__historico__descricao').innerHTML = valor;
+        }
+        // uu pra evitar  empresa 1 vazia
+        if (key == 'saida_empresa__1') {
+            if (valor == '') {
+                document.getElementById('linha__historico').innerHTML = '';
+                document.getElementById('linha__historico__descricao').innerHTML = '';
+            }
+        }
+
+
+
+        // empresa 2
+        if (key == 'name_empresa__2') {
+            document.getElementById('linha__historico2').innerHTML = valor;
+        }
+        if (key == 'cargo_empresa__2') {
+            let form = document.getElementById('linha__historico2').innerHTML;
+            let valor__campo = `<span class="bold">● ${form}</span><span class="italico"> — ${valor}</span>`;
+            document.getElementById('linha__historico2').innerHTML = valor__campo;
+        }
+        if (key == 'inicio_empresa__2') {
+            let form = document.getElementById('linha__historico2').innerHTML;
+            let valor__campo = `${form} de ${valor}`;
+            document.getElementById('linha__historico2').innerHTML = valor__campo;
+        }
+        if (key == 'saida_empresa__2') {
+            let form = document.getElementById('linha__historico2').innerHTML;
+            let valor__campo = `${form} a ${valor}`;
+            document.getElementById('linha__historico2').innerHTML = valor__campo;
+        }
+        if (key == 'resumo__empresa2') {
+            document.getElementById('linha__historico__descricao2').innerHTML = valor;
+        }
+        // uu pra evitar  empresa 2 vazia
+        if (key == 'saida_empresa__2') {
+            if (valor == '') {
+                document.getElementById('linha__historico2').innerHTML = '';
+                document.getElementById('linha__historico__descricao2').innerHTML = '';
+            }
+        }
+
+        // empresa 3
+        if (key == 'name_empresa__3') {
+            document.getElementById('linha__historico3').innerHTML = valor;
+        }
+        if (key == 'cargo_empresa__3') {
+            let form = document.getElementById('linha__historico3').innerHTML;
+            let valor__campo = `<span class="bold">● ${form}</span><span class="italico"> — ${valor}</span>`;
+            document.getElementById('linha__historico3').innerHTML = valor__campo;
+        }
+        if (key == 'inicio_empresa__3') {
+            let form = document.getElementById('linha__historico3').innerHTML;
+            let valor__campo = `${form} de ${valor}`;
+            document.getElementById('linha__historico3').innerHTML = valor__campo;
+        }
+        if (key == 'saida_empresa__3') {
+            let form = document.getElementById('linha__historico3').innerHTML;
+            let valor__campo = `${form} a ${valor}`;
+            document.getElementById('linha__historico3').innerHTML = valor__campo;
+        }
+        if (key == 'resumo__empresa3') {
+            document.getElementById('linha__historico__descricao3').innerHTML = valor;
+        }
+        // uu pra evitar  empresa 3 vazia
+        if (key == 'saida_empresa__3') {
+            if (valor == '') {
+                document.getElementById('linha__historico3').innerHTML = '';
+                document.getElementById('linha__historico__descricao3').innerHTML = '';
+            }
+        }
+        if (key == 'outros__conhecimentos') {
+            document.getElementById('linha__outros').innerHTML = valor;
+        }
+
+    }
+});
+
+let exp1 = document.getElementById('linha__historico').innerHTML;
+let exp2 = document.getElementById('linha__historico2').innerHTML;
+let exp3 = document.getElementById('linha__historico3').innerHTML;
+
+if (exp1 == '' && exp2 == '' && exp3 == '') {
+    document.getElementById('linha__historico3').innerHTML = 'Primeiro Emprego';
+}
+let resum = document.getElementById('linha__resumo').innerHTML;
+if (resum == '') {
+    document.getElementById('titulo__resumo__cv').innerHTML = '';
+}
